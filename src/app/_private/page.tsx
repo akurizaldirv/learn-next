686629b7cfeb8ae8085f7e29d46820a34ea9891f
Zaldi Irvana <akurizaldirv@gmail.com>
2024-09-08 19:10:22 +0700

    add private route

---
+++ src/app/_private/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotGonnaShow = () => {
+    return (
+        <div>
+            not gonna show, so why i wrote this text, dumb!
+        </div>
+    );
+};
+
+export default NotGonnaShow;
