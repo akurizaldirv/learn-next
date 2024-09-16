97e42abe1753459f488453dea84fefd64e1aa780
Zaldi Irvana <akurizaldirv@gmail.com>
2024-09-16 12:27:37 +0700

    add route handler: headers and cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,24 @@
-export async function GET() {
-    return new Response("Profile API");
+import {NextRequest} from "next/server";
+import {cookies} from "next/headers";
+
+export async function GET(req: NextRequest) {
+    console.log("search Params : " + req.nextUrl.searchParams);
+    console.log("Headers : " + req.headers);
+
+    // SET COOKIES
+    cookies().delete("theme"); // DELETE COOKIES
+    cookies().set("resultPerPage", "20");
+
+    // GET COOKIES
+    console.log("Cookie : " + req.cookies);
+    console.log("Theme Name : " + req.cookies?.get("theme")?.name);
+    console.log("Theme Value : " + req.cookies?.get("theme")?.value);
+
+    return new Response("Profile API", {
+         headers: {
+             ...req.headers,
+             "content-type": "text/html; charset=utf-8",
+             // "set-cookie": "theme=dark", // SET COOKIES
+         }
+    });
 }
